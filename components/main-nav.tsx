278f1d61b252e613fa8df4f0cdff45598d5f6d91
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-07-18 12:05:02 +0200

    Navbar created

---
+++ components/main-nav.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import React from "react";
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  const params = useParams();
+  const pathname = usePathname();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+  return (
+    <nav
+      className={(cn("flex items-center space-x-4 lg:space-x-6"), className)}
+    >
+      {routes.map((route, index) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
