8ba124ee44ec1bb08c6c855b45dc20f650d8214c
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-07-23 11:08:09 +0200

    Fixed route issue

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,21 +1,31 @@
 "use client";
 
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Plus } from "lucide-react";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data?: any;
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const params = useParams();
+  const router = useRouter();
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
-          description="Manage billboards for you store"
-        ></Heading>
-        <Button>
-          <Plus className="mr-2 h-4 w-4" />
-          Add New
+          title={`Billboards (0)`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
