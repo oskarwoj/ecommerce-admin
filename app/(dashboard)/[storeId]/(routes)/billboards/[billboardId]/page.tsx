ad92589a04994fe2488e05f71d779fc0d8e64d85
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-07-23 11:37:57 +0200

    Billboards Page Fixed

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import prismadb from "@/lib/prismadb";
+
+import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({
   params,
@@ -12,7 +13,13 @@ const BillboardPage = async ({
     },
   });
 
-  return <div>Existing Billboard: {billboard?.label}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage;
