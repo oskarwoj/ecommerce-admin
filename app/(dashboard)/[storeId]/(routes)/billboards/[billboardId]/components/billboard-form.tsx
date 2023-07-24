cb9ed9d1e3bfc616e9327e9e1022f898234063c6
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-07-24 11:02:08 +0200

    Billboard Form

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,6 +23,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { Heading } from "@/components/ui/heading";
 import { AlertModal } from "@/components/modals/alert-modal";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -133,12 +134,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
               <FormItem>
                 <FormLabel>Background image</FormLabel>
                 <FormControl>
-                  {/* <ImageUpload
+                  <ImageUpload
                     value={field.value ? [field.value] : []}
                     disabled={loading}
                     onChange={(url) => field.onChange(url)}
                     onRemove={() => field.onChange("")}
-                  /> */}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -69,6 +69,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               variant="secondary"
             >
               <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
             </Button>
           );
         }}
