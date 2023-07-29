554622c2e0dc2b24c84689e8646eb657b37f0031
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-07-29 13:41:56 +0200

    Added Table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,10 +7,12 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-interface BillboardClientProps {
-  data?: any;
-}
+import { DataTable } from "@/components/ui/data-table";
+import { BillboardColumn, columns } from "./columns";
 
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
 export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
@@ -19,7 +21,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (0)`}
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -29,6 +31,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -20,7 +20,7 @@ const MainNav = ({
     {
       href: `/${params.storeId}/billboards`,
       label: "Billboards",
-      active: pathname === `/${params.storeId}/billboard`,
+      active: pathname === `/${params.storeId}/billboards`,
     },
     {
       href: `/${params.storeId}/settings`,
