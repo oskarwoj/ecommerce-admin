2a0fc1aa1d61464d56d162ec1c64876e5c58924c
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-31 15:21:16 +0200

    Added DropMenu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,33 +5,33 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
-import UserMenu from "./navbar/UserMenu";
+import DropMenu from "./navbar/DropMenu";
 
 const Navbar = async () => {
-	const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-	if (!currentUser) {
-		redirect("/");
-	}
+  if (!currentUser) {
+    redirect("/");
+  }
 
-	const stores = await prismadb.store.findMany({
-		where: {
-			userId: currentUser.id,
-		},
-	});
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId: currentUser.id,
+    },
+  });
 
-	return (
-		<div className="border-b">
-			<div className="flex h-16 items-center px-4">
-				<StoreSwitcher items={stores} />
-				<MainNav className="mx-6" />
-				<div className="ml-auto flex items-center space-x-4">
-					<ThemeToggle />
-					<UserMenu currentUser={currentUser} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <DropMenu currentUser={currentUser} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
